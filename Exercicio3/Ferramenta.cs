e0ba648ebd4ee8ecfd29e3515cd332fd7dc29b7b
Rafael Mello Barbosa da Silva <138245045+RafaelMelloBarbosadaSilva@users.noreply.github.com>
2025-10-09 23:53:02 -0300

    Create Ferramenta.cs

---
+++ Exercicio3/Ferramenta.cs
@@ -0,0 +1,12 @@
+namespace ExerciciosPOO.Exercicio3
+{
+    class Ferramenta
+    {
+        public string Nome { get; set; }
+
+        public Ferramenta(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
