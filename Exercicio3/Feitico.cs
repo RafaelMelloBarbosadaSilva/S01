604cda270ce3fdd67b0ae9e03219e1845da30b60
Rafael Mello Barbosa da Silva <138245045+RafaelMelloBarbosadaSilva@users.noreply.github.com>
2025-10-09 23:52:34 -0300

    Create Feitico.cs

---
+++ Exercicio3/Feitico.cs
@@ -0,0 +1,12 @@
+namespace ExerciciosPOO.Exercicio3
+{
+    class Feitico
+    {
+        public string Nome { get; set; }
+
+        public Feitico(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
